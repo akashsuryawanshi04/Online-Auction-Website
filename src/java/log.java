79ac892a5a803a4f7b487734a66866fd6a59bfd3
Akash Suryawanshi <akashsuryawanshi1344@gmail.com>
2026-05-05 23:57:21 +0530

    Update log.java

--- src/java/log.java
+++ src/java/log.java
@@ -1,4 +1,10 @@
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
